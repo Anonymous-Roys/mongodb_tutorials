c8564be11ca62e3bcec75c188bbc1900b4020918
David Arhin <davidarhin2005@gmail.com>
2024-04-16 10:22:15 +0000

    retrieving all info from books collection

--- app.js
+++ app.js
@@ -22,7 +22,16 @@ connectToDb((err)=>{
 
 //Routes
 app.get('/books', (req, res) =>{
+    let books = []
     db.collection('books')
     .find()   //cursor toArray forEach
-    res.json({mssg:'Welcome to the api'})
+    .sort({author:1})
+    .forEach(book => books.push(book))
+    .then(() =>{
+        res.status(200).json(books)
+    })
+    .catch(()=>{
+        res.status(500).json({error: 'Could not fetch the documents'})
+    })
+    
 })
