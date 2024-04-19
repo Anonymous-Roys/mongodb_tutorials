39b5fdb292b36a426c2c0af119fd12c1dc5e275b
David Arhin <davidarhin2005@gmail.com>
2024-04-19 19:40:09 +0000

    post requests

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { ObjectId } = require('mongodb');
 //init app & middleware
 
 const app = express();
+app.use(express.json());
 
 //dbb connection
 let db;
@@ -56,4 +57,15 @@ app.get('/books/:id', (req,res) =>{
 })
 
 //setting up post request handler
-app.post('/books', (req,res))
+app.post('/books', (req, res) => {
+    const book = req.body
+
+    db.collection('books')
+    .insertOne(book)
+    .then(result =>{
+        res.status(201).json(result)    
+    })
+    .catch(err =>{
+        res.status(500).json({err: 'Could not create the document'})
+    })
+})
